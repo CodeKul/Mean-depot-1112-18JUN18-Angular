4281f7883a01097b4850330a1adaf3f69389b035
Kalyani Chute <melayer.kalyani@gmail.com>
2018-07-12 12:20:00 +0530

    *ngFor

--- src/app/directives/directives.component.ts
+++ src/app/directives/directives.component.ts
@@ -6,11 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./directives.component.css']
 })
 export class DirectivesComponent implements OnInit {
-
+isWell:boolean;   
   constructor() { }
   // course = [1,2];
-  viewMode ="list";
+  // viewMode ="list";
+  canSave=false;
+  courses = [
+    {id:1, name:'course1'},
+    {id:2, name:'course2'},
+    {id:3, name:'course3'}
+  ];
   ngOnInit() {
+  }
+  onAdd(){
+    this.courses.push(  {id:4, name:'course4'});
+  }
+  onRemove(course){
+   let index= this.courses.indexOf(course);
+   this.courses.splice(index,1);
+  }
+  onUpdate(course){
+    course.name = 'UPDATE';
   }
 
 }
