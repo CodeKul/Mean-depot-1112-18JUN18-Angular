9503796bf82b233cf32656df0ecdf1ea3470f063
Kalyani Chute <melayer.kalyani@gmail.com>
2018-07-23 12:03:38 +0530

    dynamic path variables and queryParms

--- src/app/second/second.component.ts
+++ src/app/second/second.component.ts
@@ -1,19 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-second',
   templateUrl: './second.component.html',
   styleUrls: ['./second.component.css']
 })
-export class SecondComponent implements OnInit {
+export class SecondComponent implements OnInit,OnDestroy {
   usNm:string;
-  constructor( private activatedRoute: ActivatedRoute) { }
+    sub: Subscription;
+   subQury:Subscription;
+  constructor( private activatedRoute: ActivatedRoute,
+    private router:Router
+  ) { }
 
   ngOnInit() {
     
-    this.usNm=this.activatedRoute.snapshot.params['usNm'];
+    // this.usNm=this.activatedRoute.snapshot.params['usNm'];
+    this.sub=this.activatedRoute.params.subscribe(prms => this.usNm = prms['usNm']);
+    this.subQury= this.router.routerState.root.queryParams.subscribe(qprms=> this.usNm+=' '+qprms['ver']);
   }
 
+  changeUsNm(usNm:string){
+    // this.usNm= usNm;
+    this.router.navigate(['second',usNm],
+    {queryParams:
+    {ver:new Date().getMilliseconds()}
+  });
  
+  }
+  ngOnDestroy(){
+    this.sub.unsubscribe();
+  }
 }
+
